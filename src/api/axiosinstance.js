28b4c287bbbda32c20a89ae3ddc14e77b17193c1
Arya <aryaaravindarya670@gmail.com>
2026-09-09 12:01:37 +0530

    axios instance changed commit

--- src/api/axiosinstance.js
+++ src/api/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://resume-server-y75b.onrender.com',
+  baseURL: 'https://resumebuilder-server-dbon.onrender.com',
   timeout: 5000
 });
 
